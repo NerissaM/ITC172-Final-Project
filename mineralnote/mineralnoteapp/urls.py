a36afc8777f812b500daa2145d42f2060d084248
Nerissa Metully <47128742+NerissaM@users.noreply.github.com>
2019-06-06 12:13:54 -0700

    Commit Model, Resource View, & form.
    https://congerprep.blogspot.com/2019/03/forms-and-form-tests.html

--- mineralnote/mineralnoteapp/urls.py
+++ mineralnote/mineralnoteapp/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns=[
     path('', views.index, name='index'),
+    path('gettypes/', views.gettypes, name='types'),
+    path('getresource/', views.getresource, name='products'),
+    path('resourcedetails/<int:id>', views.resourcedetails, name='resourcedetails'),
+    path('newProduct/', views.newProduct, name='newproduct'),
 ]
--- mineralnote/mineralnoteapp/views.py
+++ mineralnote/mineralnoteapp/views.py
@@ -1,6 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import ProductType, Product, Review
+from .forms import ProductForm
 
 # Create your views here.
 
 def index (request):
     return render(request, 'mineralnoteapp/index.html')
+
+def gettypes(request):
+    type_list=ProductType.objects.all()
+    return render(request, 'mineralnoteapp/types.html' ,{'type_list' : type_list})
+
+def getresource(request):
+    products_list=Product.objects.all()
+    return render(request, 'mineralnoteapp/resource.html', {'products_list': products_list})
+
+def resourcedetails(request, id):
+    res=get_object_or_404(Product, pk=id)
+    discount=res.memberdiscount
+    reviews=Review.objects.filter(product=id).count()
+    context={
+        'res' : res,
+        'discount' : discount,
+        'reviews' : reviews,
+    }
+    return render(request, 'mineralnoteapp/resourcedetails.html', context=context)
+
+def newProduct(request):
+     form=ProductForm
+     if request.method=='POST':
+          form=ProductForm(request.POST)
+          if form.is_valid():
+               post=form.save(commit=True)
+               post.save()
+               form=ProductForm()
+     else:
+          form=ProductForm()
+     return render(request, 'mineralnoteapp/newresource.html', {'form': form})
